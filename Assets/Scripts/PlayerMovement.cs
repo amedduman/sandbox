a0d805e9b4466a22ad95a90369889a714e251fa8
amedduman <amedduman@protonmail.com>
2023-02-04 01:23:58 +0300

    Add toolbar extender

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,38 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using Photon.Pun;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviourPunCallbacks
 {
-
-    // CharacterController characterController;
-    // float speed = 5f;
-    // Vector3 move = Vector3.zero;
-    // Vector3 velocity = Vector3.zero;
-    // float pitch = 0f;
-    // public Transform cameraPivot;
-    //
-    // // Start is called before the first frame update
-    // public void OnEnable()
-    // {
-    //     characterController = GetComponent<CharacterController>();
-    // }
-    //
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     transform.Rotate(0, Input.GetAxis("Mouse X") * 3f, 0);
-    //     pitch -= Input.GetAxis("Mouse Y") * 3f;
-    //     pitch = Mathf.Clamp(pitch, -60f, 60f);
-    //     cameraPivot.localEulerAngles = new Vector3(pitch, 0, 0);
-    //     move.x = Input.GetAxisRaw("Horizontal");
-    //     move.z = Input.GetAxisRaw("Vertical");
-    //     move = Vector3.ClampMagnitude(move, 1f);
-    //     velocity = transform.TransformVector(move) * speed;
-    //     characterController.SimpleMove(velocity);
-    // }
-    
         [SerializeField] Transform _cameraPivot;
         [Range(-5f, -25f)] [SerializeField] float gravity = -15f;
         [Range(5f, 15f)] [SerializeField] float movementSpeed = 10f;
@@ -47,11 +20,13 @@ public class PlayerMovement : MonoBehaviour
 
         private void Start()
         {
+            if (!photonView.IsMine) GetComponentInChildren<Camera>().gameObject.SetActive(false);
             cc = GetComponent<CharacterController>();
         }
         
         void Update()
         {
+            if(!photonView.IsMine) return;
             Look();
             Move();
         }
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+                
+    }
+}
